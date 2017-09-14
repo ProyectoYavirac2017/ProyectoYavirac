f4ebe622585f2d32fe5ef9daac4e1a0e922485d8
joseobando0001 <31702727+joseobando0001@users.noreply.github.com>
2017-09-14 11:16:42 -0500

    Grupo Nro 1

---
+++ ProyectoYavirac-web/src/main/java/ec/edu/yavirac/instituto/web/bean/i/Imethods.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.yavirac.instituto.web.bean.i;
+
+import javax.faces.event.ActionEvent;
+
+/**
+ *
+ * @author luis
+ */
+public interface Imethods {
+    
+    public void add(ActionEvent evt);
+    
+    public void modify(ActionEvent evt);
+    
+    public void remove(ActionEvent evt);
+    
+}
