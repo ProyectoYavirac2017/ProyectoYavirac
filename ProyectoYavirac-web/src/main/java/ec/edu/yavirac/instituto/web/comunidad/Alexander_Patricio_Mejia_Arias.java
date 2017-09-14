80890dec92523a730afefdced59c4b42d205b12c
Andrez-Flores <marlon-flores-87@hotmail.com>
2017-09-14 09:12:45 -0500

    Andrez flores web

--- ProyectoYavirac-web/src/main/java/ec/edu/yavirac/instituto/web/comunidad/Alexander_Patricio_Mejia_Arias.java
+++ ProyectoYavirac-web/src/main/java/ec/edu/yavirac/instituto/web/comunidad/Alexander_Patricio_Mejia_Arias.java
@@ -12,5 +12,6 @@ package ec.edu.yavirac.instituto.web.comunidad;
 public class Alexander_Patricio_Mejia_Arias {
     String Alexander_Patricio_Mejia_Arias;
     String SantiagODanielRecaldeGomez;
+    String Andrez_Flores;
     
 }
