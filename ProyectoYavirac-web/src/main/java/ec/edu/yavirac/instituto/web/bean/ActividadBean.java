bf530552ccfcffda904c8e71cf2ec8ba99e3b698
ivan16luis <ivanlugiz16@yahoo.com>
2017-09-14 15:42:34 -0500

    proyecto parte web habilitada

--- ProyectoYavirac-web/src/main/java/ec/edu/yavirac/instituto/web/bean/ActividadBean.java
+++ ProyectoYavirac-web/src/main/java/ec/edu/yavirac/instituto/web/bean/ActividadBean.java
@@ -7,8 +7,14 @@ package ec.edu.yavirac.instituto.web.bean;
 
 import ec.edu.yavirac.instituto.modelo.Actividad;
 import ec.edu.yavirac.instituto.controlador.ActividadControlador;
+import ec.edu.yavirac.instituto.controlador.FaseControlador;
+import ec.edu.yavirac.instituto.controlador.ProyectoControlador;
+import ec.edu.yavirac.instituto.modelo.Fase;
+import ec.edu.yavirac.instituto.modelo.Proyecto;
 import ec.edu.yavirac.instituto.web.bean.i.Imethods;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.event.ActionEvent;
 import javax.inject.Named;
@@ -26,21 +32,36 @@ public class ActividadBean implements Imethods, Serializable {
     private static final long serialVersionUID = -2033941921967400870L;
 
     private Actividad actividad;
+    private List<Proyecto> proyectos;
+    private Proyecto selectProy;
+    private List<Fase> fases;
+    private Fase slectFase;
+    private List<Actividad> atividades;
+            
     @Inject
     private ActividadControlador actividadControl;
+    @Inject
+    private ProyectoControlador proyectoControl;
+    @Inject
+    private FaseControlador faseControlador;
 
     @PostConstruct
     public void init() {
-      this.actividad= new Actividad();
+        this.actividad = new Actividad();
+        this.proyectos = this.proyectoControl.listProyectos();
+        if(this.proyectos == null){
+            this.proyectos = new ArrayList<>();
+        }
     }
+    
 
     @Override
     public void add(ActionEvent evt) {
         if (this.actividad != null) {
-           Boolean exito = this.actividadControl.insert(this.actividad);
-           if(exito){
-               this.init();
-           }
+            Boolean exito = this.actividadControl.insert(this.actividad);
+            if (exito) {
+                this.init();
+            }
         }
     }
 
@@ -61,5 +82,5 @@ public class ActividadBean implements Imethods, Serializable {
     public void setActividad(Actividad actividad) {
         this.actividad = actividad;
     }
- 
+
 }
