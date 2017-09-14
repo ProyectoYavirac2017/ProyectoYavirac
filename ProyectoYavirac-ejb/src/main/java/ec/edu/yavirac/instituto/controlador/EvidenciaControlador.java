bb704038a8d226ba45a2112d9f6cfcc809b3f079
Gixon1 <31931979+Gixon1@users.noreply.github.com>
2017-09-14 12:07:41 -0500

    evidencia y evaluacion

--- ProyectoYavirac-ejb/src/main/java/ec/edu/yavirac/instituto/controlador/EvidenciaControlador.java
+++ ProyectoYavirac-ejb/src/main/java/ec/edu/yavirac/instituto/controlador/EvidenciaControlador.java
@@ -5,38 +5,40 @@
  */
 package ec.edu.yavirac.instituto.controlador;
 
-import ec.edu.yavirac.instituto.i.Ievidencia;
 import ec.edu.yavirac.instituto.modelo.Evidencia;
+import ec.edu.yavirac.instituto.servicio.EvidenciaServicio;
 import ec.edu.yavirac.instituto.util.HibernateSessionHandler;
 import ec.edu.yavirac.instituto.util.HibernateUtil;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 /**
  *
  * @author Labs-DECC
  */
-public class EvidenciaControlador  implements Ievidencia,Serializable{
-      private static final long serialVersionUID = -7260505214139170751L;
+@Stateless
+@LocalBean
+public class EvidenciaControlador implements Serializable{
+    private static final long serialVersionUID = -7260505214139170751L;
     @Inject
-    private EvidenciaControlador evidenciaControlador; 
+    private EvidenciaServicio evidenciaServicio; 
     private HibernateUtil util = new HibernateUtil();
     
     @PostConstruct
     public void init() {
         util.init();
     }
-
-    @Override
-    public Boolean insert(Evidencia evidencia, HibernateUtil util) {
+    public Boolean insert(Evidencia evidencia) {
         Boolean exito = false;
         HibernateSessionHandler hss = new HibernateSessionHandler(util);
         Exception delegateException = null;
         try {
             if (evidencia != null) {
-                exito = evidenciaControlador.insert(evidencia, util);
+                exito = evidenciaServicio.insert(evidencia, util);
             }
         } catch (Exception ex) {
             // // log.level.error("insert : " + ex.getMessage());
@@ -54,14 +56,14 @@ public class EvidenciaControlador  implements Ievidencia,Serializable{
         return exito;
     }
 
-    @Override
-    public Boolean update(Evidencia evidencia, HibernateUtil util) {
-         Boolean exito = false;
+
+    public Boolean update(Evidencia evidencia) {
+        Boolean exito = false;
         HibernateSessionHandler hss = new HibernateSessionHandler(util);
         Exception delegateException = null;
         try {
             if (evidencia != null) {
-                exito = evidenciaControlador.update(evidencia, util);
+                exito = evidenciaServicio.update(evidencia, util);
             }
         } catch (Exception ex) {
             // // log.level.error("insert : " + ex.getMessage());
@@ -79,14 +81,13 @@ public class EvidenciaControlador  implements Ievidencia,Serializable{
         return exito;
     }
 
-    @Override
-    public Evidencia findById(Evidencia evidencia, HibernateUtil util) {
+    public Evidencia findById(Evidencia evidencia) {
         Evidencia findmEvidencia = null;
         HibernateSessionHandler hss = new HibernateSessionHandler(util);
         Exception delegateException = null;
         try {
             if (evidencia != null) {
-                findmEvidencia = evidenciaControlador.findById(evidencia, util);
+                findmEvidencia = evidenciaServicio.findById(evidencia, util);
             }
         } catch (Exception ex) {
             // log.level.error("FindUserLogin : " + ex.getMessage());
@@ -104,13 +105,12 @@ public class EvidenciaControlador  implements Ievidencia,Serializable{
         return findmEvidencia;
     }
 
-    @Override
-    public List<Evidencia> listEvidencias(HibernateUtil util) {
-         List<Evidencia> mEvidencia = null;
+    public List<Evidencia> listEvidencias() {
+        List<Evidencia> mEvidencia = null;
         HibernateSessionHandler hss = new HibernateSessionHandler(util);
         Exception delegateException = null;
         try {
-            mEvidencia = evidenciaControlador.listEvidencias(util);
+            mEvidencia = evidenciaServicio.listEvidencias(util);
         } catch (Exception ex) {
             // log.level.error("GetListClientes : " + ex.getMessage());
             delegateException = ex;
@@ -126,6 +126,4 @@ public class EvidenciaControlador  implements Ievidencia,Serializable{
         }
         return mEvidencia;
     }
-    
-    
 }
