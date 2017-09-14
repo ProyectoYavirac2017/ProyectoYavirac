d1c41d6b57cee10ef42369216f379fae9e57974d
cris58 <31702711+cris58@users.noreply.github.com>
2017-09-13 20:28:26 -0500

    cristian

---
+++ ProyectoYavirac-web/src/main/java/ec/edu/yavirac/instituto/web/comunidad/Christian_Alexis_Lopez_Changoluisa.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.yavirac.instituto.web.comunidad;
+
+/**
+ *
+ * @author DELL
+ */
+public class Christian_Alexis_Lopez_Changoluisa {
+    
+}
