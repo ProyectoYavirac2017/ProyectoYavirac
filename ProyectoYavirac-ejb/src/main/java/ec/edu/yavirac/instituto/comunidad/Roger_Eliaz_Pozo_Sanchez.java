bcfec09a342ad9dfca90bff559902875c7910d1a
kuky1993 <31934577+kuky1993@users.noreply.github.com>
2017-09-13 20:25:18 -0500

    roger pozo

---
+++ ProyectoYavirac-ejb/src/main/java/ec/edu/yavirac/instituto/comunidad/Roger_Eliaz_Pozo_Sanchez.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.yavirac.instituto.comunidad;
+
+/**
+ *
+ * @author user
+ */
+public class Roger_Eliaz_Pozo_Sanchez {
+    
+}
