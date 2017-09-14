ec70afcf8e42eb0feb071c5bc07704d612757acb
mishell2096 <31936364+mishell2096@users.noreply.github.com>
2017-09-14 08:47:05 -0500

    :)

--- ProyectoYavirac-web/src/main/java/ec/edu/yavirac/instituto/web/comunidad/Monica_Mishell_Lumitaxi_Quilligana.java
+++ ProyectoYavirac-web/src/main/java/ec/edu/yavirac/instituto/web/comunidad/Monica_Mishell_Lumitaxi_Quilligana.java
@@ -11,4 +11,8 @@ package ec.edu.yavirac.instituto.web.comunidad;
  */
 public class Monica_Mishell_Lumitaxi_Quilligana {
     
+     String Nombre = "Mishell ";
+    String Apellido = "Llumitaxi";
+    
+   
 }
