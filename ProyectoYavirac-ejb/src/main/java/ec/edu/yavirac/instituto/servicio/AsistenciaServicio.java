7c65f08b90ce35bd320d7be95a2625795e75a2c0
JOSEOSTI <31852473+JOSEOSTI@users.noreply.github.com>
2017-09-14 10:32:59 -0500

    Grupo Nro 1

--- ProyectoYavirac-ejb/src/main/java/ec/edu/yavirac/instituto/servicio/AsistenciaServicio.java
+++ ProyectoYavirac-ejb/src/main/java/ec/edu/yavirac/instituto/servicio/AsistenciaServicio.java
@@ -5,10 +5,108 @@
  */
 package ec.edu.yavirac.instituto.servicio;
 
+import ec.edu.yavirac.instituto.dao.DAOServices;
+import ec.edu.yavirac.instituto.dao.QueryParameter;
+import ec.edu.yavirac.instituto.i.Iasistencia;
+import ec.edu.yavirac.instituto.modelo.Asistencia;
+import ec.edu.yavirac.instituto.util.HibernateUtil;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Labs-DECC
  */
-public class AsistenciaServicio {
-    
+public class AsistenciaServicio implements Iasistencia {
+  
+    @Override
+    public Boolean insert(Asistencia asistencia, HibernateUtil util) {
+        Boolean exito = false;
+        try {
+            DAOServices ds = new DAOServices(util.
+                    getSessionFactory().getCurrentSession());
+            if (asistencia != null) {
+                ds.save(asistencia);
+                exito = true;
+            }
+        } catch (Exception e) {
+            System.out.println(">> ERROR " + e.getMessage());
+            exito = false;
+        }
+        return exito;
+
+    }
+
+    @Override
+    public Boolean update(Asistencia asistencia, HibernateUtil util) {
+
+        Boolean exito = false;
+        try {
+            DAOServices ds = new DAOServices(util.
+                    getSessionFactory().getCurrentSession());
+            if (asistencia != null) {
+                ds.update(asistencia);
+                exito = true;
+            }
+        } catch (Exception e) {
+            System.out.println(">> ERROR " + e.getMessage());
+            exito = false;
+        }
+        return exito;
+
+    }
+
+    @Override
+    public Asistencia findById(Asistencia asistencia, HibernateUtil util) {
+
+        Asistencia findmAsistencia = null;
+        try {
+            DAOServices ds = new DAOServices(util.
+                    getSessionFactory().getCurrentSession());
+
+            QueryParameter query_1 = new QueryParameter(QueryParameter.$TYPE_WHERE);
+            query_1.setColumnName("ID_ASISTENCIA");
+            query_1.setWhereClause("=");
+            query_1.setValue(asistencia.getIdAsistencia());
+
+            List parameList = new ArrayList();
+            parameList.add(query_1);
+
+            List<Asistencia> listClients = ds.customQuery(parameList, Asistencia.class);
+
+            if (!listClients.isEmpty()) {
+                findmAsistencia = listClients.get(0);
+            }
+
+        } catch (Exception ex) {
+            // log.level.info("findById : " + ex.getMessage());
+        }
+
+        return findmAsistencia;
+
+    }
+
+    @Override
+    public List<Asistencia> listAsistencias(HibernateUtil util) {
+ List<Asistencia> mAsistencias = null;
+        try {
+            
+            DAOServices ds = new DAOServices(util.
+                    getSessionFactory().getCurrentSession());
+            
+            List parameList = new ArrayList();
+            
+            List<Asistencia> listAsistencias = ds.customQuery(parameList, Asistencia.class);
+            if (!listAsistencias.isEmpty()) {
+                mAsistencias = (List<Asistencia>) listAsistencias;
+            }
+        } catch (Exception ex) {
+            // log.level.info("getListAsistencias : " + ex.getMessage());
+        }
+
+        return mAsistencias;
+
+
+    }
+
 }
