f3c1116bca2976f5f15674ab22d7592a6ecaf972
erikaOG <eortizg@institutobenitojuarez.edu.ec>
2017-09-15 11:21:57 -0500

    Arreglando

--- ProyectoYavirac-web/src/main/java/ec/edu/yavirac/instituto/web/bean/ProyectoBean.java
+++ ProyectoYavirac-web/src/main/java/ec/edu/yavirac/instituto/web/bean/ProyectoBean.java
@@ -9,17 +9,11 @@ import ec.edu.yavirac.instituto.controlador.ProyectoControlador;
 import ec.edu.yavirac.instituto.modelo.Proyecto;
 import ec.edu.yavirac.instituto.web.bean.i.Imethods;
 import java.io.Serializable;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import org.primefaces.context.RequestContext;
-import org.primefaces.event.SelectEvent;
 
 /**
  *
@@ -28,7 +22,9 @@ import org.primefaces.event.SelectEvent;
 @Named(value = "proyectoBean")
 @ViewScoped
 public class ProyectoBean implements Imethods, Serializable {
-           
+    
+    private static final long serialVersionUID = -2033941921967400870L;
+    
     private Proyecto proyecto;
     @Inject
     private ProyectoControlador proyectoControl;
@@ -41,26 +37,13 @@ public class ProyectoBean implements Imethods, Serializable {
     @Override
     public void add(ActionEvent evt) {
         if (this.proyecto != null) {
-           Boolean exito = this.proyectoControl.insert(this.proyecto);
+           Boolean exito = this.proyectoControl.insert(proyecto);
            if(exito){
                this.init();
            }
         }
     }
-    /*/Código para calendario
-     public void onDateSelect(SelectEvent event) {
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
-    }
-     
-    public void click() {
-        RequestContext requestContext = RequestContext.getCurrentInstance();
-         
-        requestContext.update("form:display");
-        requestContext.execute("PF('dlg').show()");
-    }
-    Código para calendario*/
+    
     @Override
     public void modify(ActionEvent evt) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
